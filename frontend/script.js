7b4ee51a08cac75fee0cc8e5f56d83b1ea0044fa
shaesung01-byte <shaesung01@gmail.com>
2026-01-21 13:23:55 -0500

    added list with colleges for results

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,1 @@
 
-document.querySelectorAll(".input_field").forEach(pair => {
-  const checkbox = pair.querySelector('input[type="checkbox"]');
-  const input = pair.querySelector('input[type="text"]');
-  checkbox.addEventListener("change", () => {
-    input.disabled = !checkbox.checked;
-  });
-});
-
